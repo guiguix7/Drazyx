4d4a2baa6dce74b7dc461e2e7a7e73fe1281348e
guiguix7 <guilherme.andraz@etec.sp.gov.br>
2026-09-07 18:51:42 -0300

    adiciona @vercel/analytics como dependência e integra no componente App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { AudioPlayerProvider } from "./components/Audioplayer";
+import { Analytics } from "@vercel/analytics/next"
 
 import Home from "./pages/Home";
 import Music from "./pages/Music";
@@ -36,6 +37,7 @@ export default function App() {
           </Routes>
         </main>
         <Footer />
+        <Analytics />
       </div>
     </AudioPlayerProvider>
   );
